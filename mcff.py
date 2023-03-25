5e6e650cd1fe2ef4745b9c9b885e6e5aa9f2beaf
FengAB2 <1255575131@qq.com>
2023-03-25 17:23:24 +0800

    1

---
+++ mcff.py
@@ -0,0 +1,10 @@
+# import librosa
+#
+# # 加载音频文件
+# y, sr = librosa.load('audio.wav', sr=44100)  # 采样率为44100Hz
+#
+# # 提取MFCC特征
+# mfcc = librosa.feature.mfcc(y=y, sr=sr, n_fft=1024, hop_length=int(sr/30), n_mfcc=40)
+
+if __name__ == '__main__':
+    pass
